a1533cdee2dffa7c6ae12674cf56a43930e07fc5
cronkubine <eatoneer@g.ucla.edu>
2025-04-12 15:24:44 -0700

    feat: added Navbar navigation to Resources page

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./resources.module.css";
+
+export default function Resources(){
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.header}>Resources</h1>
+        </div>
+    )
+}
