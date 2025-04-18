c889984e935544cbcc1e7f8c371f91274250ee61
EthanDao4444 <ethandao4444@gmail.com>
2025-04-17 17:18:51 -0700

    feat: created Modal for Home page/Events page

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React from 'react';
+import styles from './Modal.module.css';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.modalOverlay} onClick={onClose}>
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+        <button className={styles.modalClose} onClick={onClose}>X</button>
+        <div className={styles.modalInnerFrame}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
