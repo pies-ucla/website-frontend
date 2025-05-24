15544ffc72712608dde9dfc1ac054d5fc06531d7
Ethan Dao <miserable@Ethans-MacBook-Pro.local>
2025-05-23 17:57:34 -0700

    feat: Alumni page now contacts backend properly

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,6 +1,8 @@
 // app/api/auth/callback/route.ts
+import { cookies } from "next/headers";
 
 export async function POST(request: Request) {
+    const cookieStore = await cookies();
     try {
         const { code, redirect_uri } = await request.json();
         const response = await fetch("http://localhost:8000/token/get/", {
@@ -13,6 +15,15 @@ export async function POST(request: Request) {
         });
 
         const data = await response.json();
+        cookieStore.set({
+            name: 'access_token',
+            value: data.access,
+            httpOnly: true,
+            secure: true,
+            sameSite: 'lax',
+            path: '/',
+            maxAge: 60 * 60 * 24
+        });
 
         return new Response(JSON.stringify(data), {
         status: response.status,
